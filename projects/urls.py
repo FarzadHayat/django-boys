c9d57b98ccdcda5323e83a3d16af57c0d585c2a7
Farzad Hayat <farzad.hayat@outlook.com>
2023-12-08 15:55:14 +1000

    added project detail page. added ability to add new, edit, and delete project in site. added css.

--- projects/urls.py
+++ projects/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.project_list, name="project_list")]
+urlpatterns = [
+    path("", views.project_list, name="project_list"),
+    path("project/new/", views.project_new, name="project_new"),
+    path("project/<int:pk>/", views.project_detail, name="project_detail"),
+    path("project/<int:pk>/edit/", views.project_edit, name="project_edit"),
+    path("project/<int:pk>/delete/", views.project_delete, name="project_delete"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Project
+from .forms import ProjectForm
 
 
 def project_list(request):
@@ -8,3 +9,47 @@ def project_list(request):
         "published_date"
     )
     return render(request, "projects/project_list.html", {"projects": projects})
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, "projects/project_detail.html", {"project": project})
+
+
+def project_new(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.author = request.user
+            project.published_date = timezone.now()
+            project.save()
+            return redirect("project_detail", pk=project.pk)
+    else:
+        form = ProjectForm()
+    return render(request, "projects/project_edit.html", {"form": form})
+
+
+def project_edit(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.user != project.author:
+        return redirect("project_detail", pk)
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.author = request.user
+            project.published_date = timezone.now()
+            project.save()
+            return redirect("project_detail", pk=project.pk)
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, "projects/project_edit.html", {"form": form})
+
+
+def project_delete(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.user != project.author:
+        return redirect("project_detail", pk)
+    project.delete()
+    return redirect("project_list")
